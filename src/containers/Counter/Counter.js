a014a8189eefd2461263ad274eb0a2506d03a29e
FrancaTM <francatm@outlook.com>
2020-01-26 17:24:26 -0300

    updating state immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -22,6 +22,16 @@ class Counter extends Component {
           label="Subtract 15"
           clicked={this.props.onSubtractCounter}
         />
+
+        <hr />
+        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <ul>
+          {this.props.storedResults.map(storedResult => (
+            <li key={storedResult.id} onClick={this.props.onDeleteResult}>
+              {storedResult.value}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
@@ -29,7 +39,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
   return {
-    ctr: state.counter
+    ctr: state.counter,
+    storedResults: state.results
   };
 };
 
@@ -38,7 +49,9 @@ const mapDispatchToProps = dispatch => {
     onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
     onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
     onAddCounter: () => dispatch({ type: "ADD", val: 10 }),
-    onSubtractCounter: () => dispatch({ type: "SUBTRACT", val: 15 })
+    onSubtractCounter: () => dispatch({ type: "SUBTRACT", val: 15 }),
+    onStoreResult: () => dispatch({ type: "STORE_RESULT" }),
+    onDeleteResult: () => dispatch({ type: "DELETE_RESULT" })
   };
 };
 
