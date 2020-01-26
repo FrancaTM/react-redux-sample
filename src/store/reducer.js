f50f798a88cade3983ac50d221530dcc0bf3b5af
FrancaTM <francatm@outlook.com>
2020-01-26 17:39:42 -0300

    updating arrays immutably

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -29,6 +29,18 @@ const reducer = (state = initialState, action) => {
         ...state,
         results: state.results.concat({ id: new Date(), value: state.counter })
       };
+    case "DELETE_RESULT":
+      // const id = 2;
+      // const newArray = [...state.results];
+      // newArray.splice(id, 1);
+
+      const updatedArray = state.results.filter(
+        result => result.id !== action.resultElementId
+      );
+      return {
+        ...state,
+        results: updatedArray
+      };
 
     default:
       return state;
